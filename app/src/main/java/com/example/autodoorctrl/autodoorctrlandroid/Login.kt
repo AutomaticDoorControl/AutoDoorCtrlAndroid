b3f058ff67a06ea09043ff4ba56da9952dea2524
Matilda Rehm <mathilde.rehm@gmail.com>
2019-01-26 23:59:55 -0500

    completed splash & working on formatting initial login page

---
+++ app/src/main/java/com/example/autodoorctrl/autodoorctrlandroid/Login.kt
@@ -0,0 +1,30 @@
+package com.example.autodoorctrl.autodoorctrlandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+    private var username: EditText? = null
+    private var password: EditText? = null
+    private var pass: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+
+        hideNavBar()
+//        overridePendingTransition(R.anim.splashenter, R.anim.splashexit)
+    }
+
+    private fun hideNavBar() {
+        this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+}
