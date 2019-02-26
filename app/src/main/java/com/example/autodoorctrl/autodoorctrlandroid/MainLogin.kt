947b2c15cfa86807db5f398e66e69c6ad9cb309b
karthikimayavaramban <scorpiokarth@gmail.com>
2019-02-26 16:48:03 -0500

    App Fixes and Biometric Additions
    Due to an extra intent filter in the Android Manifest, an extra instance of the application was being installed for a different activity. Fixed this bug and also changed the logo of the app to make it more identifiable. Also added function to check if phone has biometric support.

--- app/src/main/java/com/example/autodoorctrl/autodoorctrlandroid/MainLogin.kt
+++ app/src/main/java/com/example/autodoorctrl/autodoorctrlandroid/MainLogin.kt
@@ -1,5 +1,6 @@
 package com.example.autodoorctrl.autodoorctrlandroid
 
+import android.Manifest
 import android.annotation.TargetApi
 import android.content.DialogInterface
 import android.hardware.biometrics.BiometricPrompt
@@ -9,6 +10,14 @@ import android.view.View
 import android.view.animation.AlphaAnimation
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import android.content.pm.PackageManager
+import android.Manifest.permission
+import android.Manifest.permission.USE_BIOMETRIC
+import androidx.core.app.ActivityCompat
+import android.app.KeyguardManager
+import android.content.Context
+
 
 class MainLogin : AppCompatActivity() {
     private val opacityClick: AlphaAnimation = AlphaAnimation(0.8f, 0.4f)
@@ -17,7 +26,7 @@ class MainLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_login)
         hideNavBar()
-
+        checkBiometricSupport()
         val settingsIcon = findViewById<ImageView>(R.id.gear)
         settingsIcon.setOnClickListener { it.startAnimation(opacityClick) }
     }
@@ -29,11 +38,43 @@ class MainLogin : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 
+    private fun notifyUser(message: String) {
+        Toast.makeText(
+            this,
+            message,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun checkBiometricSupport(): Boolean {
+
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        val packageManager = this.packageManager
+
+        if (!keyguardManager.isKeyguardSecure) {
+            notifyUser("Lock screen security not enabled in Settings")
+            return false
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.USE_BIOMETRIC
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            notifyUser("Fingerprint authentication permission not enabled")
+            return false
+        }
+
+        return if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            true
+        } else true
+
+    }
 
 
-
-
-    @TargetApi(Build.VERSION_CODES.P)
+/*    @TargetApi(Build.VERSION_CODES.P)
     private fun displayBiometricPrompt(biometricCallback:BiometricCallback) {
         BiometricPrompt.Builder(context)
             .setTitle(title)
@@ -46,5 +87,5 @@ class MainLogin : AppCompatActivity() {
             })
             .build()
     }
-
+*/
 }
