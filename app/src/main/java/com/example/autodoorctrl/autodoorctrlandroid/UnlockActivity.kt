12a7ef8e539f0508ac80dadc946dcce3d61481c7
karthikimayavaramban <scorpiokarth@gmail.com>
2019-04-02 17:14:36 -0400

    Unlock Activity
    Created Activity that will eventually send the bluetooth command to the door

---
+++ app/src/main/java/com/example/autodoorctrl/autodoorctrlandroid/UnlockActivity.kt
@@ -0,0 +1,12 @@
+package com.example.autodoorctrl.autodoorctrlandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnlockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unlock)
+    }
+}
